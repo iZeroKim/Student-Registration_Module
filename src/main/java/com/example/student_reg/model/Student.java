c62b5812ebc06924ec678948bfb2471416b3f8d1
iZeroKim <kimgichuru6@gmail.com>
2022-03-10 18:21:36 +0300

    Use lombok to generate getters, setters and student constructor

--- src/main/java/com/example/student_reg/model/Student.java
+++ src/main/java/com/example/student_reg/model/Student.java
@@ -1,11 +1,20 @@
 package com.example.student_reg.model;
 
-import com.example.student_reg.others.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Student {
     int id;
     String firstname;
     String lastname;
-    Gender gender;
+    String email;
+    String gender;
     int age;
+
 }
