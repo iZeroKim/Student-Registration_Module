7a01d8cfc2c37ff259076c61139d2e056497b70c
iZeroKim <kimgichuru6@gmail.com>
2022-03-10 18:34:37 +0300

    Set up students table using lombok

--- src/main/java/com/example/student_reg/model/Student.java
+++ src/main/java/com/example/student_reg/model/Student.java
@@ -5,16 +5,26 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "students")
 public class Student {
-    int id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+
+    @Column(name = "first_name")
     String firstname;
+
+    @Column(name = "last_name")
     String lastname;
-    String email;
+    String   email;
     String gender;
     int age;
-
 }
