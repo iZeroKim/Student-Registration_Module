442e3e7f1289c5c2dab3abcaf632ac44ceb2351a
iZeroKim <kimgichuru6@gmail.com>
2022-03-10 18:00:53 +0300

    Initial project setup

---
+++ src/main/java/com/example/student_reg/model/Student.java
@@ -0,0 +1,11 @@
+package com.example.student_reg.model;
+
+import com.example.student_reg.others.Gender;
+
+public class Student {
+    int id;
+    String firstname;
+    String lastname;
+    Gender gender;
+    int age;
+}
---
+++ src/main/java/com/example/student_reg/others/Gender.java
@@ -0,0 +1,5 @@
+package com.example.student_reg.others;
+
+public enum Gender {
+    MALE, FEMALE
+}
